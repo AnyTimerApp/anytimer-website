1bc2558d74c4b592158925de32cd18a00c92d090
abitmarty <martijn.totte@gmail.com>
2026-07-29 14:56:31 +0200

    Updated all images to webp

--- src/app/pages/blog/blog-posts.data.ts
+++ src/app/pages/blog/blog-posts.data.ts
@@ -48,7 +48,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Wat is een anytimer?',
     excerpt: 'Een anytimer is een tegoed tussen vrienden. Je verdient hem na een verloren weddenschap, een domme actie of een challenge. Je kan hem inzetten wanneer je maar wilt.',
     date: '2026-03-27',
-    image: 'assets/blogs/wat is een anytimer/Wat is een anytimer cover.png',
+    image: 'assets/blogs/wat is een anytimer/Wat is een anytimer cover.webp',
     imageAlt: 'Wat is een anytimer uitleg',
     variant: 'light'
   },
@@ -59,7 +59,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Top 10 leukste drankspellen voor een gezellige avond',
     excerpt: 'Van Mario Barf tot Kingsen en Mexen. Onze top 10 drankspellen voor een gezellige avond met vrienden. Met alle regels en tips.',
     date: '2026-04-10',
-    image: 'assets/blogs/drankspellen/mario barf.png',
+    image: 'assets/blogs/drankspellen/mario barf.webp',
     imageAlt: 'Mario Barf drankspel',
     variant: 'blue'
   },
@@ -70,7 +70,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Wat is een adtje?',
     excerpt: 'Een adtje betekent dat je een drankje in één keer leegdrinkt. Leer tips voor atten, het verschil met een shotgun en wat een rietbak is.',
     date: '2026-04-12',
-    image: 'assets/blogs/wat is een adtje/wat is een adtje.png',
+    image: 'assets/blogs/wat is een adtje/wat is een adtje.webp',
     imageAlt: 'Wat is een adtje',
     variant: 'light'
   },
@@ -81,7 +81,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Wat is de beste adt timer?',
     excerpt: 'Welke adt timer moet je kiezen. App timer, stopwatch, AdtMeister of Rubiks cube timer. Vergelijking en tips voor eerlijke atten.',
     date: '2026-04-17',
-    image: 'assets/blogs/beste timer/Any-timer in de club.png',
+    image: 'assets/blogs/beste timer/Any-timer in de club.webp',
     imageAlt: 'Any-timer in de club',
     variant: 'light'
   },
@@ -92,7 +92,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Mag je een anytimer weigeren?',
     excerpt: 'Mag je een anytimer weigeren? Lees de regels en gevolgen. Wanneer mag je nee zeggen en wat zijn de straffen.',
     date: '2026-04-21',
-    image: 'assets/blogs/anytimer weigeren/maatje adt een biertje cartoon.png',
+    image: 'assets/blogs/anytimer weigeren/maatje adt een biertje cartoon.webp',
     imageAlt: 'Anytimer weigeren cartoon',
     variant: 'light'
   },
@@ -103,7 +103,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Waarom je anytimers niet in WhatsApp of notities moet bijhouden',
     excerpt: 'WhatsApp en notities lijken handig, maar je mist het belangrijkste: versiebeheer. Lees waarom dit voor discussie zorgt en hoe je anytimers beter bijhoudt.',
     date: '2026-05-21',
-    image: 'assets/blogs/whatsapp en notities/Whatsapp phones.png',
+    image: 'assets/blogs/whatsapp en notities/Whatsapp phones.webp',
     imageAlt: 'Anytimers bijhouden in WhatsApp werkt niet door gebrek aan versiebeheer',
     variant: 'light'
   },
@@ -114,7 +114,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Top 10 leuke huisavondideeën',
     excerpt: 'Van de Zwarte Piste tot een kerstdiner midden in juli. Onze top 10 leuke huisavondideeën voor als je met je huisgenoten thuis blijft.',
     date: '2026-07-21',
-    image: 'assets/blogs/huisavond/Huisavond cover.png',
+    image: 'assets/blogs/huisavond/Huisavond cover.webp',
     imageAlt: 'Top 10 huisavondideeën',
     variant: 'light'
   },
@@ -126,7 +126,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Jeu de bier',
     excerpt: 'Jeu de bier is jeu de boules voor aan tafel met bierdopjes. Simpel behendigheidsspel, perfect voor het begin van de avond.',
     date: '2026-07-22',
-    image: 'assets/blogs/drankspel/jeu de bier.png',
+    image: 'assets/blogs/drankspel/jeu de bier.webp',
     imageAlt: 'Jeu de bier drankspel met bierdopjes',
     variant: 'light'
   },
@@ -138,7 +138,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Mario Barf',
     excerpt: 'Mario Barf is Mario Kart met een drankje naast je controller. Je moet je drankje op hebben voor je over de finish komt.',
     date: '2026-07-24',
-    image: 'assets/blogs/drankspellen/mario barf.png',
+    image: 'assets/blogs/drankspellen/mario barf.webp',
     imageAlt: 'Mario Barf drankspel met Mario Kart en drankjes naast de controller',
     variant: 'light'
   },
@@ -150,7 +150,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Trek je Bak Mania',
     excerpt: 'Trek je Bak Mania is Trackmania met een simpele straf. De slechtste coureur van de ronde trekt een bak.',
     date: '2026-07-28',
-    image: 'assets/blogs/drankspellen/trek je bak mania.png',
+    image: 'assets/blogs/drankspellen/trek je bak mania.webp',
     imageAlt: 'Trek je Bak Mania drankspel met Trackmania in hotseat mode',
     variant: 'light'
   },
@@ -162,7 +162,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: '30 Seconds',
     excerpt: 'Raad binnen 30 seconden zoveel mogelijk woorden. Als drankspel wordt elke mislukte ronde een kleine straf voor je team.',
     date: '2026-07-24',
-    image: 'assets/blogs/drankspellen/30 seconds.png',
+    image: 'assets/blogs/drankspellen/30 seconds.webp',
     imageAlt: '30 Seconds drankspel met het bordspel en een timer',
     variant: 'light'
   },
@@ -174,7 +174,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Kingsen',
     excerpt: 'Kingsen is een kaartspel waarbij elke kaart een opdracht geeft. Probeer niet degene te zijn die de laatste koning pakt.',
     date: '2026-07-24',
-    image: 'assets/blogs/drankspellen/kingsen.png',
+    image: 'assets/blogs/drankspellen/kingsen.webp',
     imageAlt: 'Kingsen drankspel met kaarten in een cirkel rond een shotglas',
     variant: 'light'
   },
@@ -186,7 +186,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Stress Pong',
     excerpt: 'Stress Pong is een snelle variant op pong waarbij twee bekers tegelijk rondgaan. Wordt je ingehaald? Dan moet je drinken.',
     date: '2026-07-24',
-    image: 'assets/blogs/drankspellen/stress pong.png',
+    image: 'assets/blogs/drankspellen/stress pong.webp',
     imageAlt: 'Stress Pong drankspel met bekers en pingpongballen',
     variant: 'light'
   },
@@ -198,7 +198,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Fuck the Dealer',
     excerpt: 'Fuck the Dealer is een kaartspel waarin iedereen de dealer probeert te pakken door de waarde van kaarten te raden.',
     date: '2026-07-24',
-    image: 'assets/blogs/drankspellen/fuck the dealer.png',
+    image: 'assets/blogs/drankspellen/fuck the dealer.webp',
     imageAlt: 'Fuck the Dealer kaartspel als drankspel',
     variant: 'light'
   },
@@ -210,7 +210,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Mexen',
     excerpt: 'Mexen is een dobbelspel met twee stenen. Voorkom de laagste worp van de ronde en let op voor de Mex.',
     date: '2026-07-24',
-    image: 'assets/blogs/drankspellen/mexen.png',
+    image: 'assets/blogs/drankspellen/mexen.webp',
     imageAlt: 'Mexen dobbelspel als drankspel',
     variant: 'light'
   },
@@ -222,7 +222,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Bakplaten',
     excerpt: 'Bakplaten is een heel simpel kaartspel. Trek een plaatje en jij bent aan de beurt om te drinken.',
     date: '2026-07-24',
-    image: 'assets/blogs/drankspellen/bakplaten.png',
+    image: 'assets/blogs/drankspellen/bakplaten.webp',
     imageAlt: 'Bakplaten kaartspel als simpel drankspel',
     variant: 'light'
   },
@@ -234,7 +234,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'De Paardenrace',
     excerpt: 'De Paardenrace is een kaartspel waarin de vier azen tegen elkaar racen. Zet slokken in en hoop dat jouw paard wint.',
     date: '2026-07-24',
-    image: 'assets/blogs/drankspellen/paardenrace.png',
+    image: 'assets/blogs/drankspellen/paardenrace.webp',
     imageAlt: 'De Paardenrace drankspel met azen als paarden',
     variant: 'light'
   },
@@ -246,7 +246,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Ring of fire',
     excerpt: 'Ring of fire is de internationale variant van Kingsen. Trek een kaart uit de ring en voer meteen de bijpassende opdracht uit.',
     date: '2026-07-28',
-    image: 'assets/blogs/drankspel/ring of fire.png',
+    image: 'assets/blogs/drankspel/ring of fire.webp',
     imageAlt: 'Ring of fire drankspel met kaarten in een ring rond een glas',
     variant: 'light'
   },
@@ -258,7 +258,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Mijnenveld',
     excerpt: 'Mijnenveld is een kaartspel waarin je een route door kaarten kiest. Stap je op een plaatje of aas, dan begin je opnieuw.',
     date: '2026-07-28',
-    image: 'assets/blogs/drankspel/mijnenveld.png',
+    image: 'assets/blogs/drankspel/mijnenveld.webp',
     imageAlt: 'Mijnenveld kaartspel met kaarten in een ruitvormig veld',
     variant: 'light'
   },
@@ -270,7 +270,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Stef stuntpiloot',
     excerpt: 'Stef stuntpiloot is een behendigheidsspel met een vliegtuigje. Verdedig je kippen en stuur Stef naar de kippen van de rest.',
     date: '2026-07-28',
-    image: 'assets/blogs/drankspel/stef stuntpiloot.png',
+    image: 'assets/blogs/drankspel/stef stuntpiloot.webp',
     imageAlt: 'Stef Stuntpiloot drankspel met het vliegtuigje dat rondjes vliegt boven de kippenhokken',
     variant: 'light'
   },
@@ -282,7 +282,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Vingeren',
     excerpt: 'Vingeren is een snel raadspel met vingers op een glas. Raad hoeveel vingers blijven liggen en kom zo uit het spel.',
     date: '2026-07-28',
-    image: 'assets/blogs/drankspel/vingeren.png',
+    image: 'assets/blogs/drankspel/vingeren.webp',
     imageAlt: 'Vingeren drankspel met vingers op de rand van een glas',
     variant: 'light'
   },
@@ -294,7 +294,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Vikingen',
     excerpt: 'Vikingen is een spel waarbij iedereen tegelijk moet opletten. Eén speler is Viking, de buren roeien en de rest maakt de zee.',
     date: '2026-07-28',
-    image: 'assets/blogs/drankspel/vikingen.png',
+    image: 'assets/blogs/drankspel/vikingen.webp',
     imageAlt: 'Vikingen drankspel met een speler als Viking en twee spelers die roeien',
     variant: 'light'
   },
@@ -306,7 +306,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Zeeslag',
     excerpt: 'Zeeslag is het bekende bordspel met een drankregel erbij. Raak je een schip van de ander, dan moet die persoon drinken.',
     date: '2026-07-28',
-    image: 'assets/blogs/drankspel/zeeslag.png',
+    image: 'assets/blogs/drankspel/zeeslag.webp',
     imageAlt: 'Zeeslag drankspel met het Zeeslag bord en pionnen',
     variant: 'light'
   },
@@ -318,7 +318,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Kusje geven',
     excerpt: 'Kusje geven is een behendigheidsspel met een speelkaart. Geef de kaart door met je mond en laat hem vooral niet vallen.',
     date: '2026-07-28',
-    image: 'assets/blogs/drankspel/kusje geven.png',
+    image: 'assets/blogs/drankspel/kusje geven.webp',
     imageAlt: 'Kusje geven drankspel met een speelkaart die van mond tot mond gaat',
     variant: 'light'
   },
@@ -330,7 +330,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Bussen',
     excerpt: 'Bussen is een kaartspel met meerdere rondes. Eerst verzamel je kaarten, daarna beslist de piramide wie de bus in moet.',
     date: '2026-07-28',
-    image: 'assets/blogs/drankspel/bussen.png',
+    image: 'assets/blogs/drankspel/bussen.webp',
     imageAlt: 'Bussen kaartspel met de piramide en de kaarten voor de bus',
     variant: 'light'
   },
@@ -342,7 +342,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Hoger lager',
     excerpt: 'Hoger lager is een simpel kaartspel waarin je steeds moet gokken. Komt de volgende kaart hoger of lager uit?',
     date: '2026-07-28',
-    image: 'assets/blogs/drankspel/hoger lager.png',
+    image: 'assets/blogs/drankspel/hoger lager.webp',
     imageAlt: 'Hoger lager kaartspel met een rij opengedraaide kaarten',
     variant: 'light'
   },
@@ -354,7 +354,7 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Krokodillenspel',
     excerpt: 'Krokodillenspel is Krokodil met Kiespijn als drankspel. Iedereen drukt om de beurt een tand in en hoopt dat de bek open blijft.',
     date: '2026-07-28',
-    image: 'assets/blogs/drankspel/krokodil.png',
+    image: 'assets/blogs/drankspel/krokodil.webp',
     imageAlt: 'Krokodillenspel met het spel Krokodil met Kiespijn en shotglazen',
     variant: 'light'
   }
